1839432afb2dda8bffbb54609d674e78c30b63b4
lazikos94 <lazikos94@gmail.com>
2022-12-21 12:23:58 +0200

    sadad

--- microservices/auth/routes/v1/JWTAuthentication.py
+++ microservices/auth/routes/v1/JWTAuthentication.py
@@ -6,59 +6,66 @@ from microservices.auth.schema.LoginBody import *
 from microservices.auth.schema.RegisterBody import *
 from microservices.auth.schema.AuthBody import *
 from config import settings
-#from core.db import getCollection
+# from core.db import getCollection
 from core.db import database
 from core.db import collection
-from core.utils import (passwordEncrypt,passwordVerify,jwtTokenEncode,jwtTokenRefresh)
+from core.utils import (passwordEncrypt, passwordVerify,
+                        jwtTokenEncode, jwtTokenRefresh)
 from uuid import uuid4
 
 jwtauthentication_router = APIRouter()
 
+
 @jwtauthentication_router.post("/login", dependencies=[])
-async def login (loginBody: LoginBody):
+async def login(loginBody: LoginBody):
     try:
-        user = database['test-database'].find_one({"email":loginBody.email})
+        user = database['test-database'].find_one({"email": loginBody.email})
         print(user['password'])
         print(loginBody.password)
-        print( passwordVerify(loginBody.password,user['password']))
-        if passwordVerify(loginBody.password,user['password']):
+        print(passwordVerify(loginBody.password, user['password']))
+        if passwordVerify(loginBody.password, user['password']):
             return {
                 "token": jwtTokenEncode(user['email']),
-                "user": loginBody.email    
+                "user": loginBody.email
             }
         return {
             "error": "Wrong login details!"
         }
-    except:
+    except Exception as inst:
+        print(type(inst))    # the exception instance
+        print(inst.args)     # arguments stored in .args
+        print(inst)
         return "Error logining in"
 
+
 @jwtauthentication_router.post("/register", dependencies=[])
-async def register (registerBody: RegisterBody):
+async def register(registerBody: RegisterBody):
     try:
         user = {
             'email': registerBody.email,
-            'username':registerBody.username,
+            'username': registerBody.username,
             'password': passwordEncrypt(registerBody.password),
         }
-        
-        already = database['test-database'].find_one({'email':user['email']})
-        if(already):
+
+        already = database['test-database'].find_one({'email': user['email']})
+        if (already):
             print('User already exists')
             return 'User already exists'
         else:
-            database['test-database'].insert_one(user).inserted_id   # saving user to database
-            user['_id'] = str (user['_id'])
+            # saving user to database
+            database['test-database'].insert_one(user).inserted_id
+            user['_id'] = str(user['_id'])
             return user
     except:
         return "Error registering"
-   
-   
-@jwtauthentication_router.post("/authenticate",dependencies=[])
-async def authenticate(authBody:AuthBody):
+
+
+@jwtauthentication_router.post("/authenticate", dependencies=[])
+async def authenticate(authBody: AuthBody):
     try:
-        auth={
-        'token':authBody.token
-       
+        auth = {
+            'token': authBody.token
+
         }
 
         check = jwtTokenDecode(auth['token'])
@@ -68,15 +75,15 @@ async def authenticate(authBody:AuthBody):
         return "Error authenticating token"
 
 
-@jwtauthentication_router.get("/db",dependencies=[])
+@jwtauthentication_router.get("/db", dependencies=[])
 async def read(req: Request):
     post = {"author": "Mike",
-        "text": "My first blog post!",
-        "tags": ["mongodb", "python", "pymongo"]}
+            "text": "My first blog post!",
+            "tags": ["mongodb", "python", "pymongo"]}
     posts = database['test-database']
     post_id = posts.insert_one(post).inserted_id
     print(post_id)
-    #created_student =  getCollection()
-    return{
-     # created_student
+    # created_student =  getCollection()
+    return {
+        # created_student
     }
